a9b7d8fa0647c501bae1b5776183a4bf30554d48
MariaLanss <maruska0990@gmail.com>
2023-07-31 22:39:56 +0300

    .

---
+++ src/main/java/ru/hogwarts/school/Hw4Cource3Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw4Cource3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw4Cource3Application.class, args);
+    }
+
+}
